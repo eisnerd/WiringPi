a22c724692ed29b4ab42c625318d5a8f2a5751f0
Eric Limpens <limpens@gmail.com>
2013-10-01 11:06:47 +0200

    Added missing autoconf requirements

--- PACKAGE/wiringPi.mk
+++ PACKAGE/wiringPi.mk
@@ -3,7 +3,7 @@ WIRINGPI_SITE = https://github.com/limpens/WiringPi/tarball/master
 WIRINGPI_INSTALL_TARGET = YES
 WIRINGPI_AUTORECONF = YES
 
-ifeq ($(BR2_PACKAGE_ERIC_WIRINGPI),y)
-TARGETS+=rgbtop
+ifeq ($(BR2_PACKAGE_WIRINGPI),y)
+TARGETS+=wiringPi
 endif
 $(eval $(autotools-package))
